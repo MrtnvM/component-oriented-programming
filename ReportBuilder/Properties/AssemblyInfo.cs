c5bd5e052399a73cfb7576070ec06025670f3340
Max Martynov <max.mrtnv@gmail.com>
2016-03-27 23:21:40 +0400

    Added component ReportBuilder

---
+++ ReportBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ReportBuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportBuilder")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a6aad157-b09b-4ea3-9859-098c5f9a293a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReportBuilder/ReportBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using iTextSharp.text;
+using Models;
+using iTextSharp.text.pdf;
+
+namespace ReportBuilder
+{
+    public partial class ReportBuilder : Component
+    {
+        public readonly int InstanceId;
+        private static int _nextInstanceId = 0;
+        private static long _classInstanceCount = 0;
+
+        private readonly string _path;
+        private readonly List<Product> _products;
+
+        public long IstanceCount => _classInstanceCount;
+
+        public ReportBuilder(IEnumerable<Product> products, string path)
+        {
+            InitializeComponent();
+
+            InstanceId = _nextInstanceId++;
+            _classInstanceCount++;
+
+            _path = path;
+            _products = products.ToList();
+        }
+
+        public void Generate()
+        {
+            var products = _products.OrderBy(p => p.Price);
+            var doc = new PdfDocument();
+            PdfWriter.GetInstance(doc, new FileStream(_path, FileMode.Create));
+            doc.Open();
+
+            WriteHeader(doc);
+            WriteReport(doc, products);
+
+            doc.Close();
+        }
+
+        private void WriteHeader(PdfDocument doc)
+        {
+            var header = new Paragraph(new Phrase("Отчет по остатку товара"));
+            header.Alignment = Element.ALIGN_CENTER;
+            header.Font.Size = 18;
+            doc.Add(header);
+        }
+
+        private void WriteReport(PdfDocument doc, IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                var p = new Paragraph(new Phrase(product.Name))
+                {
+                    Font = {Size = 16},
+                    Alignment = Element.ALIGN_LEFT
+                };
+                p.Font.SetStyle("bold");
+                doc.Add(p);
+
+                p = new Paragraph(new Phrase(product.Description))
+                {
+                    Alignment = Element.ALIGN_JUSTIFIED,
+                    Font = {Size = 14}
+                };
+                doc.Add(p);
+
+                p = new Paragraph(new Phrase(product.Price))
+                {
+                    Font = {Size = 16},
+                    Alignment = Element.ALIGN_LEFT
+                };
+                p.Font.SetStyle("bold");
+                doc.Add(p);
+
+                doc.Add(new Paragraph(""));
+            }
+        }
+
+        ~ReportBuilder()
+        {
+            _classInstanceCount--;
+        }
+    }
+}
